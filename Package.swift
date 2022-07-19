143e3b330bf0e9490021426c74aa953a730e9d14
pravin <pravinpk0707@gmail.com>
2022-07-19 14:44:08 +0530

    Remove unit test target

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .library(name: "AEPCampaignClassic", targets: ["AEPCampaignClassic"])
     ],
     dependencies: [
-        .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.2.0"))
+        .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.7.0"))
     ],
     targets: [
         .target(name: "AEPCampaignClassic",
