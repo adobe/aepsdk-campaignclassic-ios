c21b9500a5654db8c9e6e3a7a3854dbe056c8c66
pravin <pravinpk0707@gmail.com>
2022-08-02 22:34:10 -0700

    [MOB-16232] - implementation of Serializer class

--- AEPCampaignClassic/Sources/CampaignClassicXMLSerializer.swift
+++ AEPCampaignClassic/Sources/CampaignClassicXMLSerializer.swift
@@ -9,4 +9,56 @@
  OF ANY KIND, either express or implied. See the License for the specific language
  governing permissions and limitations under the License.
  */
+
 import Foundation
+import AEPServices
+
+class CampaignClassicXMLSerializer {
+
+    /// Creates xml string from the provided variant map.
+    /// - Parameter map :  a dictionary containing AnyCodable key value pairs
+    /// - Returns: string in campaign classic xml format
+    static func serializeToXMLString(map: [String: AnyCodable]) -> String {
+        var xmlString = ""
+        for(key, value) in map {
+            if let stringValue = getStringFromAnyCodable(anyCodable: value) {
+                xmlString.append("<param name=\"\(escapeString(key))\" value=\"\(escapeString(stringValue))\" />")
+            }
+        }
+        return URLEncoder.encode(value: "<additionalParams>\(xmlString)</additionalParams>")
+    }
+
+    /// Retrieves string value from AnyCodable
+    private static func getStringFromAnyCodable(anyCodable: AnyCodable) -> String? {
+        if let value = anyCodable.value {
+            switch value {
+            case is String:
+                return anyCodable.stringValue
+            case is Int:
+                if let intValue = anyCodable.intValue { return String(intValue) }
+            case is Double:
+                if let doubleValue = anyCodable.doubleValue { return String(doubleValue) }
+            case is Bool:
+                if let boolValue = anyCodable.boolValue { return String(boolValue) }
+            case is Float:
+                if let floatValue = anyCodable.floatValue { return String(floatValue) }
+            default:
+                return nil
+            }
+        }
+        return nil
+    }
+
+    /// Creates escaped string from given input string
+    private static func escapeString(_ input: String) -> String {
+        var escapedString = input
+        escapedString = escapedString.replacingOccurrences(of: "\r", with: "")
+        escapedString = escapedString.replacingOccurrences(of: "\n", with: "")
+        escapedString = escapedString.replacingOccurrences(of: "&", with: "&amp;")
+        escapedString = escapedString.replacingOccurrences(of: "\"", with: "&quot;")
+        escapedString = escapedString.replacingOccurrences(of: "'", with: "&#x27;")
+        escapedString = escapedString.replacingOccurrences(of: ">", with: "&gt;")
+        escapedString = escapedString.replacingOccurrences(of: "<", with: "&lt;")
+        return escapedString
+    }
+}
